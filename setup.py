cb540f651eb5019d7e578b08014057a9943885c3
William Fedder <fedderw@gmail.com>
2022-02-07 18:12:02 -0500

    setup directory with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of a state-level child allowance in each of Marylands new legislative districts',
+    author='William Fedder',
+    license='MIT',
+)
