969e334127b1dc6ef8789663d635340cadc281e6
fedderw <fedderw@gmail.com>
2022-03-14 19:30:43 -0400

    add 2012 districts geojson

--- notebooks/notebook_helpers.py
+++ notebooks/notebook_helpers.py
@@ -2,24 +2,33 @@ import os
 import pathlib
 
 
-def rel_location(directory_name: str = "src") -> str:
-    """Goes up until it finds the folder 'Input_Data', then it stops
+
+
+def rel_location(directory_contains: str = "src") -> str:
+    """Goes up until it finds the folder contain a file or folder specified, then it stops
     returns '' or '../' or '../../', or ... depending on how many times it had to go up"""
     path = pathlib.Path(__file__)
     num_tries = 5
     for num_up_folder in range(num_tries):
         path = path.parent
-        if directory_name in os.listdir(path):
+        if directory_contains in os.listdir(path):
             break
 
     if num_tries == num_up_folder:
         raise FileNotFoundError(
-            f"The directory '{directory_name}' could not be found in the 5"
+            f"The directory '{directory_contains}' could not be found in the 5"
             " directories above this file's location. "
         )
     location = "../" * num_up_folder
     return pathlib.Path(location)
 
+class PathToRoot:
+    def __init__(self, directory_contains: str = "src"):
+        self.directory_contains = directory_contains
+        self.from_here = rel_location(self.directory_contains)
+
+    def __call__(self, *args, **kwargs):
+        return rel_location(self.directory_contains)
 
 if __name__ == "__main__":
     print(rel_location("docs"))
